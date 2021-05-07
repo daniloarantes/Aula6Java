df0ed34e8143b83bb8265ed3151afcce1ef24364
danilo <danilo@arantes>
2021-05-06 22:56:40 -0300

    Adicionado contador

--- ProjetoTeste/src/main/java/com/daniloarantes/projetoteste/Principal.java
+++ ProjetoTeste/src/main/java/com/daniloarantes/projetoteste/Principal.java
@@ -12,5 +12,12 @@ package com.daniloarantes.projetoteste;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int cont = 0;
+        
+        while (cont <= 5){
+            System.out.println("Contando " + cont);
+            cont++;
+        }
     }
 }
